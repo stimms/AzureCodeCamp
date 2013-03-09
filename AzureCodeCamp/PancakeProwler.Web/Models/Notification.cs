e521eb019c54b479b69e04669ae34561f7936658
Simon Timms <stimms@gmail.com>
2013-03-09 00:53:51 -0700

    Adding notify dialog box

---
+++ AzureCodeCamp/PancakeProwler.Web/Models/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace PancakeProwler.Web.Models
+{
+    public class Notification
+    {
+        [ScaffoldColumn(false)]
+        public Guid Id { get; set; }
+
+        [Phone]
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+}
