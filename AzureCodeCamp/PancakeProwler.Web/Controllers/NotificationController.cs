6ed177a1801622d197bf0f5de2bebed22b0532f7
Simon Timms <stimms@gmail.com>
2013-03-09 00:32:24 -0700

    Adding modal

---
+++ AzureCodeCamp/PancakeProwler.Web/Controllers/NotificationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PancakeProwler.Web.Controllers
+{
+    public class NotificationController : Controller
+    {
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
