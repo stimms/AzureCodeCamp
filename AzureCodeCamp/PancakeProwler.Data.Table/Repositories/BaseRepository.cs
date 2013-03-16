f18c43c9e6ebe674cfa5b15fd67fc792b63086e2
Simon Timms <stimms@gmail.com>
2013-03-16 17:54:54 -0600

    Starting to add table storage

---
+++ AzureCodeCamp/PancakeProwler.Data.Table/Repositories/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace PancakeProwler.Data.Table.Repositories
+{
+    class BaseRepository
+    {
+
+    }
+}
