f808ae210d86d9fbcdf2c264953284349eef21c7
Simon Timms <stimms@gmail.com>
2013-03-24 13:05:36 -0600

    Improving meal adding UI

--- AzureCodeCamp/PancakeProwler.Data.Common/Models/Meal.cs
+++ AzureCodeCamp/PancakeProwler.Data.Common/Models/Meal.cs
@@ -13,21 +13,42 @@ namespace PancakeProwler.Data.Common.Models
         [Required]
         public string Name { get; set; }
 
+        [Display(Name="Sponsor Name")]
         public string SponsorName { get; set; }
+
+        [Display(Name="Sponsor Website")]
         public string SponsorWebSite { get; set; }
+
+        [EmailAddress]
+        [Display(Name="Sponsor EMail")]
         public string SponsorEMail { get; set; }
+
+        [Display(Name="Sponsor Twitter")]
         public string SponsorTwitter { get; set; }
 
         [Required]
+        [Display(Name="Contact Name") ]
         public string ContactName { get; set; }
 
         [EmailAddress]
+        [Display(Name="Contact EMail")]
         public string ContactEMail { get; set; }
+        [Phone]
+        [Display(Name="Contact Phone Number")]
         public string ContactPhoneNumber { get; set; }
 
         [UIHint("Textarea")]
         public string Address { get; set; }
+
+        [UIHint("Hidden")]
+        public decimal Latitude { get; set; }
+
+        [UIHint("Hidden")]
+        public decimal Longitude { get; set; }
+
         public DateTime Date { get; set; }
+
+        [UIHint("Textarea")]
         public string Description { get; set; }
 
         [UIHint("FileUpload")]
