13f2f3eeb91eaa40ec4988a2d08a79572906c75a
Simon Timms <stimms@gmail.com>
2013-04-20 17:41:32 -0600

    Adding nifty spinner during reverse geocoding

--- AzureCodeCamp/PancakeProwler.Web/Scripts/PageScripts/Meal/Create.js
+++ AzureCodeCamp/PancakeProwler.Web/Scripts/PageScripts/Meal/Create.js
@@ -11,15 +11,28 @@ var PancakeProwler;
                 ReverseGeoCoder.prototype.Init = function () {
                     var _this = this;
                     this.addressBox.blur(function () {
+                        $(".error").addClass("hidden");
+                        $(".success").addClass("hidden");
+                        $(".loading").removeClass("hidden");
                         $.ajax({
                             url: 'http://dev.virtualearth.net/REST/v1/Locations/CA/AB/-/Calgary/' + encodeURIComponent(_this.addressBox.val()) + '?maxResults=1&key=AppR9q-SG6Qtlh_6Nh6ZB2swEwxHmbbZeakhYvQAMRrYR384NFRghT5uybCR-ehX&JsonType=callback',
                             dataType: 'jsonp',
                             jsonpCallback: "jsoncallback",
                             success: function (result) {
-                                _this.latitude.val(result.resourceSets[0].resources[0].point.coordinates[0]);
-                                _this.longitude.val(result.resourceSets[0].resources[0].point.coordinates[1]);
+                                $(".loading").addClass("hidden");
+                                if(result.resourceSets[0].estimatedTotal > 0) {
+                                    _this.latitude.val(result.resourceSets[0].resources[0].point.coordinates[0]);
+                                    _this.longitude.val(result.resourceSets[0].resources[0].point.coordinates[1]);
+                                    $(".success").removeClass("hidden");
+                                } else {
+                                    $(".error").removeClass("hidden");
+                                }
                             },
-                            jsonp: 'jsonp'
+                            jsonp: 'jsonp',
+                            error: function () {
+                                $(".loading").addClass("hidden");
+                                $(".error").removeClass("hidden");
+                            }
                         });
                     });
                 };
--- AzureCodeCamp/PancakeProwler.Web/Scripts/PageScripts/Meal/Create.ts
+++ AzureCodeCamp/PancakeProwler.Web/Scripts/PageScripts/Meal/Create.ts
@@ -7,14 +7,30 @@ module PancakeProwler.Meal.Create {
         }
 
         Init() {
-            this.addressBox.blur( () => {
+            this.addressBox.blur(() => {
+                $(".error").addClass("hidden");
+                $(".success").addClass("hidden");
+                $(".loading").removeClass("hidden");
+               
                 $.ajax({
                     url: 'http://dev.virtualearth.net/REST/v1/Locations/CA/AB/-/Calgary/' + encodeURIComponent(this.addressBox.val()) + '?maxResults=1&key=AppR9q-SG6Qtlh_6Nh6ZB2swEwxHmbbZeakhYvQAMRrYR384NFRghT5uybCR-ehX&JsonType=callback',
                     dataType: 'jsonp', jsonpCallback: "jsoncallback", success: (result) => {
-                        this.latitude.val(result.resourceSets[0].resources[0].point.coordinates[0]);
-                        this.longitude.val(result.resourceSets[0].resources[0].point.coordinates[1]);
+                        $(".loading").addClass("hidden");
+                        
+                        if (result.resourceSets[0].estimatedTotal > 0) {
+                            this.latitude.val(result.resourceSets[0].resources[0].point.coordinates[0]);
+                            this.longitude.val(result.resourceSets[0].resources[0].point.coordinates[1]);
+                            $(".success").removeClass("hidden");
+                        }
+                        else {
+                            $(".error").removeClass("hidden");
+                        }
                     },
-                    jsonp: 'jsonp'
+                    jsonp: 'jsonp',
+                    error: () => {
+                        $(".loading").addClass("hidden");
+                        $(".error").removeClass("hidden");
+                    }
                 });
             });
         }
