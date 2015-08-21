18e6cc4553ae81aa6d01019e66b5fa0a5e6e962b
Kyle Baley <kyle@baley.org>
2015-08-20 20:39:53 -0400

    Give the postgres recipe repository a bootstrapper to initialize itself

--- AzureCodeCamp/PancakeProwler.Data.Postgres/Repositories/RecipeRepository.cs
+++ AzureCodeCamp/PancakeProwler.Data.Postgres/Repositories/RecipeRepository.cs
@@ -9,7 +9,7 @@ using Dapper;
 
 namespace PancakeProwler.Data.Postgres.Repositories
 {
-    public class RecipeRepository : IRecipeRepository
+    public class RecipeRepository : IRecipeRepository, IPostgresRepository
     {
         public IEnumerable<Recipe> List()
         {
@@ -38,6 +38,26 @@ namespace PancakeProwler.Data.Postgres.Repositories
 
         private string ConnectionString {
             get { return ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString; }
+        }
+
+        public void InitPostgresStorage()
+        {
+            var sql = @"DROP TABLE IF EXISTS recipes;
+CREATE TABLE recipes
+(
+  ""id"" uuid NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000'::uuid,
+  ""name"" text NOT NULL DEFAULT ''::text,
+  ""contributor"" text,
+  ""ingredients"" text,
+  ""steps"" text,
+  ""image_location"" text,
+  CONSTRAINT ""pk_recipes"" PRIMARY KEY (""id"")
+);";
+            using ( var conn = new NpgsqlConnection( ConnectionString ) )
+            {
+                conn.Open();
+                conn.Execute(sql);
+            }
         }
     }
 }
